e208c9af00be29f2a388bc1805f93b6b2f6254b5
Matt Burns <matt.burns01@gmail.com>
2022-11-02 08:44:52 -0700

    created conection js and moved code from server.js

--- server.js
+++ server.js
@@ -6,3 +6,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user:'root',
+        password:'',
+        database:'employee_db'
+    },
+    console.log(`conected to employee_db database`)
+    );
+
+    
