62303b8052937db20178479deace958ef3ec9a5a
Matt Burns <matt.burns01@gmail.com>
2022-11-02 10:06:33 -0700

    after watching video i realised that we were not useing module.exports so i moved the connection back to server and deleted the connection.js file from db folder

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require ('express');
-
+const fs = require ('fs');
 const mysql = require('mysql2');
-
 const PORT = process.env.PORT || 3001;
-
-const app = express();
+let employees = [];
+let roles = [];
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'RubberDucky',
+    database: 'schema_db'
+});
 
